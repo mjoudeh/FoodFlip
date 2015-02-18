a375635051a4531bc85db722553f2158cc3ec088
William Bolton <wbolton7@gatech.edu>
2015-02-17 19:56:58 -0500

    Added autocomplete for buildings in submit form.

--- app/src/main/java/com/gt/foodflip/SubmitScreenActivity.java
+++ app/src/main/java/com/gt/foodflip/SubmitScreenActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -11,8 +13,6 @@ import android.widget.Toast;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
-
-import java.util.HashMap;
 
 /**
  * Created by beer on 2/10/2015.
@@ -23,13 +23,19 @@ public class SubmitScreenActivity extends Activity {
     ImageButton account_button_submit_form;
     EditText text_description;
     EditText text_type;
-    HashMap<String, String> entriesList;
-    //FFDBController ffdbController = new FFDBController(this);
+    private AutoCompleteTextView buildingsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+
+        String[] buildings = getResources().getStringArray(R.array.list_of_buildings);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                buildings);
+
+        buildingsList = (AutoCompleteTextView) findViewById(R.id.autocompletetext_building);
         back_button_submit_form = (ImageButton) findViewById(R.id.back_button_submit_form);
         submit_button_submit_form = (ImageButton) findViewById(R.id.submit_button_submit_form);
         account_button_submit_form = (ImageButton) findViewById(R.id.account_button_submit_form);
@@ -37,6 +43,8 @@ public class SubmitScreenActivity extends Activity {
         text_type = (EditText) findViewById(R.id.text_type);
         back_button_submit_form.setOnClickListener(mainScreen);
         submit_button_submit_form.setOnClickListener(submitFood);
+
+        buildingsList.setAdapter(adapter);
     }
 
     View.OnClickListener mainScreen = new View.OnClickListener() {
@@ -48,36 +56,32 @@ public class SubmitScreenActivity extends Activity {
 
     View.OnClickListener submitFood = new View.OnClickListener() {
         public void onClick(View v) {
-            entriesList = new HashMap<String, String>();
-            entriesList.put("FoodType", text_type.getText().toString());
-            entriesList.put("FoodDescription", text_description.getText().toString());
-            String foodT = text_type.getText().toString();
-            String foodD = text_description.getText().toString();
-            //ffdbController.insertEntry(entriesList);
-            // Create AsycHttpClient object
-            AsyncHttpClient client = new AsyncHttpClient();
-            // Http Request Params Object
-            RequestParams params = new RequestParams();
-            params.put("FoodType", foodT);
-            params.put("FoodDescription", foodD);
-            // Make Http call to insertentry.php
-            client.post("http://128.61.126.152/foodflip/insertentry.php", params, new AsyncHttpResponseHandler() {
-                @Override
-                public void onSuccess(String response) {
+        // Create AsycHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        // Http Request Params Object
+        RequestParams params = new RequestParams();
+        params.put("FoodType", text_type.getText().toString());
+        params.put("FoodDescription", text_description.getText().toString());
+        // Make Http call to insertentry.php
+        client.post("http://128.61.126.152/foodflip/insertentry.php", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(String response) {
+                System.out.println(response);
+            }
+            // When error occured
+            @Override
+            public void onFailure(int statusCode, Throwable error, String content) {
+                if (statusCode == 404) {
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                } else if (statusCode == 500) {
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet]",
+                            Toast.LENGTH_LONG).show();
                 }
-                // When error occured
-                @Override
-                public void onFailure(int statusCode, Throwable error, String content) {
-                    if (statusCode == 404) {
-                        Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
-                    } else if (statusCode == 500) {
-                        Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet]",
-                                Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
+            }
+        });
         }
     };
 }
